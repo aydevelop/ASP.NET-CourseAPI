4d57cd79e487ae429df242a3717b98d4f89a6bf7
aydevelop <ay.develop1@gmail.com>
2020-07-17 22:39:44 +0300

    api/authors

--- CourseAPI/Controllers/AuthorsController.cs
+++ CourseAPI/Controllers/AuthorsController.cs
@@ -1,16 +1,28 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CourseAPI.Controllers
 {
+    [ApiController]
+    [Route("api/authors")]
     public class AuthorsController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
         {
-            return View();
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authors = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authors);
         }
     }
 }
