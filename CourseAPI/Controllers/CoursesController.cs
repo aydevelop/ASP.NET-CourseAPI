6d3e69f94294bd7d96d67645ef2914095881d6d6
aydevelop <ay.develop1@gmail.com>
2020-07-18 14:21:55 +0300

    api/authors/{authorId}/courses

---
+++ CourseAPI/Controllers/CoursesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CourseAPI.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseAPI.Controllers
+{
+    [ApiController]
+    [Route("api/authors/{authorId}/courses")]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public CoursesController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public IActionResult GetCoursesForAuthor(Guid authorId)
+        {
+            var coursesForAuthorFromRepo = _courseLibraryRepository.GetCourses(authorId);
+            var res = _mapper.Map<IEnumerable<CourseDto>>(coursesForAuthorFromRepo);
+
+            if (res.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(res);
+        }
+    }
+}
---
+++ CourseAPI/Profiles/CoursesProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CourseAPI.Models;
+using CourseLibrary.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseAPI.Profiles
+{
+    public class CoursesProfile : Profile
+    {
+        public CoursesProfile()
+        {
+            CreateMap<Course, CourseDto>();
+        }
+    }
+}
